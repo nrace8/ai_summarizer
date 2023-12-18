2d5e3c3f398e30d56fef6ef64710f05f2d0b795c
nrace8 <113190174+nrace8@users.noreply.github.com>
2023-12-18 18:53:56 -0500

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Hero from './components/Hero.jsx';
+import Demo from './components/Demo.jsx';
+
+import './App.css';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+	return (
+		<main>
+			<div className="main">
+				<div className="gradient" />
+				<div className="app">
+					<Hero />
+					<Demo />
+				</div>
+			</div>
+		</main>
+	);
+};
 
-export default App
+export default App;
