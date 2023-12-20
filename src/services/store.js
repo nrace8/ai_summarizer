33234414d4b2add1c4fe201d961a021353684ee5
nrace8 <113190174+nrace8@users.noreply.github.com>
2023-12-20 16:00:02 -0500

    redux2

--- src/services/store.js
+++ src/services/store.js
@@ -1,3 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { articleApi } from "./article";
 
-export const store = configureStore
+export const store = configureStore({
+    reducer: {
+        [articleApi.reducerPatch]: articleApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
+})
